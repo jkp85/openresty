a507ab720ce325358db9a07651cb47a1e96c0bf3
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-09-15 16:30:15 +0200

    Rstudio base
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- server.lua
+++ server.lua
@@ -1,31 +1,9 @@
-local redis = require "resty.redis"
-local cjson = require "cjson"
-local red = redis:new()
-
-red:set_timeout(1000) -- 1 sec
-
-local host, port = string.match(os.getenv('REDIS_URL'), '%a+://(.+):(%d+)')
-local ok, err = red:connect(host, port)
-if not ok then
-    ngx.log(ngx.ERR, "failed to connect to redis: ", err)
-    return ngx.exit(500)
+local reg = "/servers/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/endpoint/"
+if ngx.var.http_referer ~= nil and ngx.re.find(ngx.var.http_referer, reg, "ioj") ~= nil then
+	local res = {}
+	for match in (ngx.var.http_referer.."/"):gmatch("(.-)/") do
+		table.insert(res, match)
+	end
+	local path = "/"..table.concat({table.unpack(res, 4, 11)}, "/")
+	ngx.redirect(path..ngx.var.request_uri, 301)
 end
-
-local data, err = red:get("server_details_" .. ngx.var.serverId)
-if not data or data == ngx.null then
-    ngx.log(ngx.ERR, "failed to fetch details: ", err)
-    if ngx.var.serverType == 'model' then
-        local resp_text = cjson.encode({
-            schema_version = "0.1",
-            model_version = "1.0",
-            timestamp = os.date("%Y-%m-%dT%TZ", os.time()),
-            status = "error",
-            reason = "model does not exist or is not running"
-        })
-        ngx.header['content-type'] = 'application/json; charset=utf-8'
-        ngx.print(resp_text)
-    end
-    return ngx.exit(404)
-end
-local parsed = cjson.decode(data)
-ngx.var.server = string.format('%s:%d', parsed['private_ip'], parsed['port'])
